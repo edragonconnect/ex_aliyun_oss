0e3a2eefe38574409928a7226f8820205388c0b5
Kevin Pan <kevin.pan@edragonconnect.com>
2024-07-02 21:27:20 +0800

    fix: compile warning

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ExAliyunOss.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :eex],
       mod: {ExAliyunOss.Application, []}
     ]
   end
